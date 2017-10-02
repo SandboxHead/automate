94c0f6d2f1efc75646364ad83674f561c5dc8a21
Aniket Kumar <cs5160398@cse.iitd.ac.in>
2017-10-02 19:22:40 +0530

    Open multiple social networking sites at a time

---
+++ webscraping/socialnetworks.py
@@ -0,0 +1,6 @@
+import webbrowser
+
+#chrome = webbrowser.get('C:\Program Files (x86)\Google\Chrome\Application\chrome.exe')
+webbrowser.open_new_tab("www.facebook.com")
+webbrowser.open_new_tab("web.whatsapp.com")
+webbrowser.open_new_tab("gmail.com")
